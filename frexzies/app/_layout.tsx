6b7744d15a7b67a00e2390b56c2c41c141b6fa3c
samfrexz619 <samfrexz69@gmail.com>
2024-08-07 18:28:30 +0100

    add frexzies-app dir with assets

---
+++ frexzies/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf')
+  })
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+// rnfes to get boilerplate
---
+++ frexzies/app/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Frexzies</Text>
+      <StatusBar style="auto" />
+      <Link href='/profile' style={{ color: 'blue' }}>Go to profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
